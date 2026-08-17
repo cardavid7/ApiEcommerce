16e50633f6ec1006549009cc84311a5345184d78
cardavid7 <cardavid7@gmail.com>
2026-08-17 16:12:51 -0500

    identity models, configurations and migrations

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using System.Text;
 using ApiEcommerce.Constants;
+using ApiEcommerce.Models;
 using ApiEcommerce.OpenApi;
 using ApiEcommerce.Repository;
 using ApiEcommerce.Repository.IRepository;
@@ -31,7 +32,7 @@ builder.Services.AddAutoMapper(cfg =>
     cfg.AddMaps(typeof(Program).Assembly);
 });
 //Identity
-builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddDefaultTokenProviders();
 
