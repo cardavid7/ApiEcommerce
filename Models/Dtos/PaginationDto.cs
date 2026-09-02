9275b5265074b61cb123bb9fdfeb869bbdfb6b48
cardavid7 <cardavid7@gmail.com>
2026-09-01 23:30:15 -0500

    validation for pageSize and pageNumber

--- Models/Dtos/PaginationDto.cs
+++ Models/Dtos/PaginationDto.cs
@@ -3,13 +3,32 @@ namespace ApiEcommerce.Models.Dtos;
 public class PaginationDto
 {
     private const int MaxPageSize = 50;
+    private int _pageNumber = 1;
     private int _pageSize = 5;
 
-    public int PageNumber { get; set; } = 1;
+    public int PageNumber
+    {
+        get => _pageNumber;
+        set => _pageNumber = value < 1 ? 1 : value;
+    }
 
     public int PageSize
     {
         get => _pageSize;
-        set => _pageSize = value > MaxPageSize ? MaxPageSize : value;
+        set
+        {
+            if (value < 1)
+            {
+                _pageSize = 1;
+            }
+            else if (value > MaxPageSize)
+            {
+                _pageSize = MaxPageSize;
+            }
+            else
+            {
+                _pageSize = value;
+            }
+        }
     }
 }
