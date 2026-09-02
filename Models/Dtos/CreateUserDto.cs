f123c4b01d45e1e225fbc09f43d4ee67aab44380
cardavid7 <cardavid7@gmail.com>
2026-09-02 01:44:25 -0500

    validat email at CreateUserDto

--- Models/Dtos/CreateUserDto.cs
+++ Models/Dtos/CreateUserDto.cs
@@ -9,6 +9,7 @@ public class CreateUserDto
     public string Name { get; set; } = string.Empty;
 
     [Required(ErrorMessage = "Username is required")]
+    [EmailAddress(ErrorMessage = "Username must be a valid email address")]
     public string UserName { get; set; } = string.Empty;
 
     [Required(ErrorMessage = "Password is required")]
--- Models/Dtos/UserLoginDto.cs
+++ Models/Dtos/UserLoginDto.cs
@@ -6,6 +6,7 @@ namespace ApiEcommerce.Models.Dtos;
 public class UserLoginDto
 {
     [Required(ErrorMessage = "Username is required")]
+    [EmailAddress(ErrorMessage = "Username must be a valid email address")]
     public string? UserName { get; set; }
 
     [Required(ErrorMessage = "Password is required")]
